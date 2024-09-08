47abb954fbed57a1fff4dbf1933605bb24f5c10e
Juan Pablo Sarmiento <pablowritescode@gmail.com>
2024-09-08 17:33:50 -0400

    Added basic graph

--- server/main.ts
+++ server/main.ts
@@ -4,11 +4,23 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
-app.use(express.json());
+app.use(express.json()); // eslint-disable-line
+
+const ELEMENTS = [
+  {
+    data: { id: 'one', label: 'Node 1', something: 'woah' },
+    position: { x: 40, y: 40 },
+  },
+  {
+    data: { id: 'two', label: 'Node 2', something: 'woah2' },
+    position: { x: 100, y: 20 },
+  },
+  { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } },
+];
 
 // Basic route
-app.get('/api/getData', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+app.get('/api/data', (_: Request, res: Response) => {
+  res.json(ELEMENTS);
 });
 
 // Start the server
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import cytoscape from 'cytoscape';
+import * as React from 'react';
+import CytoscapeComponent from 'react-cytoscapejs';
+
+const ELEMENTS = [
+  {
+    data: { id: 'one', label: 'Node 1', something: 'woah1' },
+    position: { x: 40, y: 40 },
+  },
+  {
+    data: { id: 'two', label: 'Node 2', something: 'woah2' },
+    position: { x: 100, y: 20 },
+  },
+  { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } },
+];
+
 export function App(): JSX.Element {
+  const cyRef = React.useRef<cytoscape.Core | null>(null);
+
+  React.useEffect(() => {
+    const cy = cyRef.current;
+
+    // add event handlers
+    if (cy) {
+      cy.on('click', 'node', (event: cytoscape.EventObjectNode) => {
+        const node = event.target;
+        console.log(`clicked: ${node.data('something')}`);
+      });
+    }
+  }, []);
+
   return (
-    <p>
-      Test
-    </p>
-  )
+    <div className="flex h-screen w-screen items-center justify-center">
+      <CytoscapeComponent
+        cy={(cy) => {
+          cyRef.current = cy;
+        }}
+        elements={ELEMENTS}
+        style={{ width: '600px', height: '600px' }}
+      />
+    </div>
+  );
 }
