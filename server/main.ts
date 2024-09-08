e2c36c02c77e2c33c31a153e5d79bdf4432144ae
Juan Pablo Sarmiento <pablowritescode@gmail.com>
2024-09-08 16:52:35 -0400

    Added server

--- server/main.ts
+++ server/main.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Basic route
-app.get('/', (req: Request, res: Response) => {
+app.get('/api/getData', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
 
