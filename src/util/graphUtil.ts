c2da8b0f99d2a44c374309d32921b1b45572e301
Juan Pablo Sarmiento <pablowritescode@gmail.com>
2024-09-08 21:24:30 -0400

    Only show members that have edges

--- src/util/graphUtil.ts
+++ src/util/graphUtil.ts
@@ -7,7 +7,18 @@ export function campGraphToCytoscapeElements(
   const { members, edges } = campGraph;
   const allElements: cytoscape.ElementDefinition[] = [];
 
+  const membersWithEdges: Set<string> = new Set();
+  edges.forEach((edge) => {
+    membersWithEdges.add(edge.source);
+    membersWithEdges.add(edge.target);
+  });
+
   members.forEach((member) => {
+    // don't show members with no edges
+    if (!membersWithEdges.has(member.fullName)) {
+      return;
+    }
+
     // cytoscape expects unique ids for nodes, so we set the member's fullName
     // to be the id
     allElements.push({
