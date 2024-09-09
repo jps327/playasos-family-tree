50067d5df4ae9aa3897554534f0c7be799e7dc5e
Juan Pablo Sarmiento <pablowritescode@gmail.com>
2024-09-08 20:45:33 -0400

    Refactored graph layout into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import cytoscape from 'cytoscape';
 import * as React from 'react';
-import CytoscapeComponent from 'react-cytoscapejs';
 import { useQuery } from '@tanstack/react-query';
 import { CampGraph, CampMember } from './util/types';
-import { campGraphToCytoscapeElements } from './util/graphUtil';
 import { Drawer } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { MemberProfile } from './components/MemberProfile';
+import { GraphLayout } from './components/GraphLayout';
 
 function useData() {
   return useQuery({
@@ -24,69 +23,26 @@ function useData() {
 }
 
 export function App(): JSX.Element {
-  const [cyAPI, setCyAPI] = React.useState<cytoscape.Core | null>(null);
   const { data: campGraph } = useData();
   const [isDrawerOpened, drawerActions] = useDisclosure(false);
-  const [selectedNode, setSelectedNode] = React.useState<
-    cytoscape.NodeSingular | undefined
+  const [selectedCampMember, setSelectedCampMember] = React.useState<
+    CampMember | undefined
   >();
 
-  React.useEffect(() => {
-    // add event handlers
-    const handleNodeClick = (event: cytoscape.EventObjectNode) => {
-      const node = event.target;
-      setSelectedNode(node);
+  const onCampMemberSelect = React.useCallback(
+    (campMember: CampMember) => {
+      setSelectedCampMember(campMember);
       drawerActions.open();
-    };
-
-    cyAPI?.on('click', 'node', handleNodeClick);
-
-    // clean up event handlers
-    return () => {
-      cyAPI?.off('click', 'node', handleNodeClick);
-    };
-  }, [cyAPI, drawerActions]);
-
-  const selectedCampMember: CampMember | undefined = React.useMemo(() => {
-    return selectedNode?.data();
-  }, [selectedNode]);
-
-  // convert camp graph to cytoscape elements
-  const cytoscapeElements = React.useMemo(() => {
-    return campGraph ? campGraphToCytoscapeElements(campGraph) : [];
-  }, [campGraph]);
+    },
+    [drawerActions],
+  );
 
   return (
     <div className="relative flex h-screen w-screen items-center justify-center">
       {campGraph ? (
-        <CytoscapeComponent
-          cy={setCyAPI}
-          elements={cytoscapeElements}
-          style={{ width: '100%', height: '100%' }}
-          layout={{ name: 'concentric' }}
-          stylesheet={[
-            {
-              selector: 'node',
-              style: {
-                label: 'data(fullName)', // Use fullName as label for nodes
-                width: 50,
-                height: 50,
-                'background-color': '#61bffc',
-                'text-valign': 'center',
-                'text-halign': 'center',
-              },
-            },
-            {
-              selector: 'edge',
-              style: {
-                width: 2,
-                'line-color': '#ccc',
-                'target-arrow-color': '#ccc', // Set arrow color to match the line
-                'target-arrow-shape': 'triangle', // Define the arrow shape
-                'curve-style': 'bezier', // Use a smooth curved line for the edge
-              },
-            },
-          ]}
+        <GraphLayout
+          campGraph={campGraph}
+          onCampMemberSelect={onCampMemberSelect}
         />
       ) : null}
       <Drawer
